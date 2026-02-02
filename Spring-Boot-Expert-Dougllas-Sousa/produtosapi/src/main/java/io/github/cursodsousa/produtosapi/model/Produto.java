12e4ce61213a7002dbdee6e2c3cf11642a97acf1
pedrohtserra <pepeserra2023@gmail.com>
2026-02-02 17:42:41 -0300

    Primera Pratica do Curso Spring Boot adicionado

---
+++ Spring-Boot-Expert-Dougllas-Sousa/produtosapi/src/main/java/io/github/cursodsousa/produtosapi/model/Produto.java
@@ -0,0 +1,68 @@
+package io.github.cursodsousa.produtosapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//  POJO -> Plain Old Java Object
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "desc")
+    private String desc;
+
+    @Column(name = "preco")
+    private Double preco;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", desc='" + desc + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
